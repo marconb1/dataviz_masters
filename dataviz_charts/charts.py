b623248905c16cf96391c8f1d6cfe5c6acdaa899
Marco <marco@assertive.ai>
2024-06-24 01:16:12 +0000

    adding map, new about page and colors

--- dataviz_charts/charts.py
+++ dataviz_charts/charts.py
@@ -4,10 +4,9 @@ import re
 import pandas as pd
 import plotly.graph_objects as go
 from typing import List
-
-
+import plotly.express as px
 pd.set_option('display.max_columns', None)
-
+pd.set_option('display.max_seq_items', None)
 
 
 def extract_text_between_quotes(string):
@@ -41,9 +40,10 @@ def clean_salary_range(salary: str) -> str:
     return salary.replace('mÃªs', 'mês')
 
 def clean_data(df: DataFrame) -> DataFrame:
+    df.columns = [extract_text_between_quotes(column_name)[1] if len(extract_text_between_quotes(column_name)) > 1 else column_name for column_name in list(df.columns)]
 
-    df.columns = [extract_text_between_quotes(column_name)[1] for column_name in list(df.columns)]
     df = df[df['Genero'] != 'Prefiro não informar']
+    df = df[df['Genero'] != 'Outro']
     df['Nivel de Ensino'] = df['Nivel de Ensino'].apply(substituir_nivel_ensino)
     df['Faixa salarial'] = df['Faixa salarial'].apply(lambda x: clean_salary_range(x) if pd.notna(x) else x)
 
@@ -57,18 +57,25 @@ def plot_age_distribution_by_gender(df: DataFrame) -> None:
     :param path: Caminho para o arquivo CSV.
     :param encoding: Codificação do arquivo CSV.
     """
+    print(f'colunas {df.columns}')
+
     data_filtered = df.dropna(subset=['Idade', 'Faixa idade', 'Genero'])
 
     data_aggregated = data_filtered.groupby(['Faixa idade', 'Genero']).size().reset_index(name='count')
 
     fig = go.Figure()
 
+    color_dict = {
+    'Masculino': '#0A77BE', 
+    'Feminino': '#FF008E',
+    }
     for gender in data_aggregated['Genero'].unique():
         df_gender = data_aggregated[data_aggregated['Genero'] == gender]
         fig.add_trace(go.Bar(
             x=df_gender['Faixa idade'],
             y=df_gender['count'],
-            name=gender
+            name=gender,
+            marker_color=color_dict[gender]
         ))
 
     fig.update_layout(
@@ -88,7 +95,6 @@ def plot_age_distribution_with_proportions_by_gender(df: pd.DataFrame) -> None:
     :param df: DataFrame contendo os dados.
     """
     data_filtered = df.dropna(subset=['Idade', 'Faixa idade', 'Genero'])
-
     total_por_genero = data_filtered.groupby('Genero').size()
     #fazendo proporção
     data_aggregated = data_filtered.groupby(['Faixa idade', 'Genero']).size().reset_index(name='count')
@@ -96,23 +102,31 @@ def plot_age_distribution_with_proportions_by_gender(df: pd.DataFrame) -> None:
 
     fig = go.Figure()
 
+    color_dict = {
+    'Masculino': '#0A77BE', 
+    'Feminino': '#FF008E',
+    }
+
     for gender in data_aggregated['Genero'].unique():
         df_gender = data_aggregated[data_aggregated['Genero'] == gender]
         fig.add_trace(go.Bar(
             x=df_gender['Faixa idade'],
             y=df_gender['percentage'],
-            name=gender
+            name=gender,
+            marker_color=color_dict[gender]
         ))
 
     fig.update_layout(
-        title='Distribuição de Idade por Gênero (Proporções)',
+        title='Distribuição de Idade por Gênero (Normalizado)',
         xaxis_title='Faixa de Idade',
         yaxis_title='Porcentagem',
-        barmode='group'
+        barmode='group',
+        font=dict(
+            family='Arial',
+            size=12 
+        )
     )
-
     return fig
-
 
 
 def plot_age_distribution_pie_chart(df: DataFrame, genero: str) -> None:
@@ -130,6 +144,19 @@ def plot_age_distribution_pie_chart(df: DataFrame, genero: str) -> None:
 
     data_aggregated = data_filtered.groupby('Faixa idade').size().reset_index(name='count')
 
+    colors_dict = {
+        '17-21': '#25384B',
+        '22-24': '#799951',
+        '25-29': '#0A77BE',
+        '30-34': '#FF008E', 
+        '35-39': '#c2c2f0',
+        '40-44': '#ffb3e6',
+        '45-49': '#c2f0c2',
+        '50-54': '#ff6666',
+    }
+
+    colors = [colors_dict[faixa] for faixa in data_aggregated['Faixa idade']]
+
     fig = go.Figure(data=[go.Pie(
         labels=data_aggregated['Faixa idade'],
         values=data_aggregated['count'],
@@ -137,13 +164,14 @@ def plot_age_distribution_pie_chart(df: DataFrame, genero: str) -> None:
         textinfo='percent+label',
         pull=[0.1 for _ in range(len(data_aggregated))],
         hole=0.4,
-        sort=False
+        sort=False,
+        marker=dict(colors=colors)
     )])
 
     fig.update_layout(
         title=f'Distribuição de Faixa de Idade para {genero}',
     )
-
+    fig.update_traces(rotation=40)
     return fig
 
 
@@ -162,16 +190,22 @@ def plot_stacked_bar_percentage_education_level_by_gender(df: DataFrame) -> None
 
     fig = go.Figure()
 
+    color_dict = {
+    'Masculino': '#0A77BE', 
+    'Feminino': '#FF008E',
+    }
+
     for gender in data_aggregated['Genero'].unique():
         df_gender = data_aggregated[data_aggregated['Genero'] == gender]
         fig.add_trace(go.Bar(
             x=df_gender['Nivel de Ensino'],
             y=df_gender['percentage'],
-            name=gender
+            name=gender,
+            marker_color=color_dict[gender] 
         ))
 
     fig.update_layout(
-        title='Nível de Ensino por Gênero (Proporções)',
+        title='Nível de Ensino por Gênero (Normalizado)',
         xaxis_title='Nível de Ensino',
         yaxis_title='Porcentagem',
         barmode='stack'
@@ -180,7 +214,110 @@ def plot_stacked_bar_percentage_education_level_by_gender(df: DataFrame) -> None
     return fig
 
 
-# prompt: utilizando nosso dataframe, criar agora a relação de faixa salarial por gênero
+def plot_mapa(df, faixa_salarial):
+    df_filtered = df[df['Faixa salarial'] == faixa_salarial]
+
+    df_count = df_filtered.groupby('uf onde mora')['Faixa salarial'].count().reset_index()
+    df_count.columns = ['uf onde mora', 'Faixa salarial Count']
+
+    geojson_url = "https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson"
+
+    fig = go.Figure(go.Choropleth(
+        geojson=geojson_url,
+        locations=df_count['uf onde mora'],
+        z=df_count['Faixa salarial Count'],
+        featureidkey="properties.sigla",
+        colorscale="Viridis",
+        marker_line_width=0.5,
+        marker_line_color='white',
+        colorbar_title="Contagem de Faixas Salariais",
+    ))
+
+    fig.update_geos(
+        visible=False,
+        fitbounds="locations",
+        showcountries=True,
+        countrycolor="Black",
+        showcoastlines=True,
+        coastlinecolor="Black",
+        showland=True,
+        landcolor="lightgray"
+    )
+
+    fig.update_layout(
+        title_text=f"Distribuição de '{faixa_salarial}' por Estado",
+        title_x=0.5,
+        geo=dict(
+            showframe=False,
+            showcoastlines=True,
+            coastlinecolor="Black"
+        )
+    )
+
+    return fig
+
+
+def plot_mapa(df, faixa_salarial,genero):
+    if genero == 'todos':
+        df_filtered = df[df['Faixa salarial'] == faixa_salarial]
+    else:
+        df_filtered = df[(df['Faixa salarial'] == faixa_salarial) & (df['Genero'] == genero)]
+
+
+    df_count = df_filtered.groupby('uf onde mora')['Faixa salarial'].count().reset_index()
+    df_count.columns = ['uf onde mora', 'Faixa salarial Count']
+
+    all_states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+    df_count = df_count.set_index('uf onde mora').reindex(all_states, fill_value=0).reset_index()
+
+    geojson_url = "https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson"
+
+    fig = go.Figure(data=go.Choropleth(
+        geojson=geojson_url,
+        locations=df_count['uf onde mora'],
+        z=df_count['Faixa salarial Count'],
+        featureidkey="properties.sigla",
+        colorscale=[[0, 'lightblue'], [1, 'darkblue']], 
+        autocolorscale=False,
+        reversescale=False,
+        marker_line_color='darkgray',
+        marker_line_width=0.5,
+        colorbar_tickprefix='',
+        colorbar_title='Faixa Salarial<br>Count',
+    ))
+
+    fig.update_geos(
+        visible=False,
+        fitbounds="locations",
+        showcountries=True,
+        countrycolor="Black",
+        showcoastlines=True,
+        coastlinecolor="Black",
+        showland=True,
+        landcolor="lightgray",
+        projection_type='mercator',
+        lonaxis_range=[-75, -30], 
+        lataxis_range=[-35, 5], 
+    )
+
+    fig.update_layout(
+        title_text=f"Distribuição da faixa salarial escolhida: <b>'{faixa_salarial}'<b> por Estado",
+        geo=dict(
+            showframe=False,
+            showcoastlines=True,
+            coastlinecolor="Black",
+            bgcolor='rgba(0,0,0,0)' 
+        ),
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)', 
+        width=1000,
+        height=700  
+    )
+
+
+
+    return fig
+
 
 def plot_stacked_bar_percentage_salary_range_by_gender(df: DataFrame) -> None:
     """
@@ -189,12 +326,36 @@ def plot_stacked_bar_percentage_salary_range_by_gender(df: DataFrame) -> None:
     :param df: DataFrame contendo os dados.
     """
     data_filtered = df.dropna(subset=['Genero', 'Faixa salarial'])
-
+    
     total_por_genero = data_filtered.groupby('Genero').size()
 
     data_aggregated = data_filtered.groupby(['Faixa salarial', 'Genero']).size().reset_index(name='count')
     data_aggregated['percentage'] = data_aggregated.apply(lambda row: (row['count'] / total_por_genero[row['Genero']]) * 100, axis=1)
 
+    faixa_salarial_order = [
+    'Menos de R$ 1.000/mês',
+    'de R$ 101/mês a R$ 2.000/mês ',
+    'de R$ 1.001/mês a R$ 2.000/mês',
+    'de R$ 2.001/mês a R$ 3.000/mês',
+    'de R$ 3.001/mês a R$ 4.000/mês',
+    'de R$ 4.001/mês a R$ 6.000/mês',
+    'de R$ 6.001/mês a R$ 8.000/mês',
+    'de R$ 8.001/mês a R$ 12.000/mês',
+    'de R$ 12.001/mês a R$ 16.000/mês',
+    'de R$ 16.001/mês a R$ 20.000/mês',
+    'de R$ 20.001/mês a R$ 25.000/mês',
+    'de R$ 25.001/mês a R$ 30.000/mês',
+    'de R$ 30.001/mês a R$ 40.000/mês',
+    'Acima de R$ 40.001/mês'
+]
+    
+    color_dict = {
+    'Masculino': '#0A77BE', 
+    'Feminino': '#FF008E',
+    }
+
+    data_aggregated['Faixa salarial'] = pd.Categorical(data_aggregated['Faixa salarial'], categories=faixa_salarial_order, ordered=True)
+    data_aggregated = data_aggregated.sort_values('Faixa salarial')
     fig = go.Figure()
 
     for gender in data_aggregated['Genero'].unique():
@@ -202,7 +363,8 @@ def plot_stacked_bar_percentage_salary_range_by_gender(df: DataFrame) -> None:
         fig.add_trace(go.Bar(
             x=df_gender['Faixa salarial'],
             y=df_gender['percentage'],
-            name=gender
+            name=gender,
+            marker_color=color_dict[gender] 
         ))
 
     fig.update_layout(
--- dataviz_charts/main.py
+++ dataviz_charts/main.py
@@ -4,24 +4,77 @@ from pandas import DataFrame
 import re
 import pandas as pd
 import plotly.graph_objects as go
+import base64
+import os
 from typing import List
 from charts import (plot_age_distribution_by_gender,
                     plot_age_distribution_with_proportions_by_gender,
                     plot_stacked_bar_percentage_education_level_by_gender,
                     plot_age_distribution_pie_chart, 
                     plot_stacked_bar_percentage_salary_range_by_gender,
-                    clean_data)
+                    clean_data,
+                    plot_mapa)
 
-path = 'dataviz_charts/data/State_of_data_2022.csv'
+path = 'dataviz_charts/data/State_of_data_BR_2023_Kaggle - df_survey_2023.csv'
 data = pd.read_csv(path)
 st.set_page_config(layout="wide")
 df_clean = clean_data(data)
 
-options = st.sidebar.radio('DataViz: Data Gender Study:', ['Age Distribution', 'Education level', 'Salary Distribution'])
+options = st.sidebar.radio('Trabalho de Visualização de dados:', ['Sobre','Salário','Idade', 'Educação'])
 
-if options == 'Age Distribution':
-    _,col_title = st.columns([1, 1.2])
-    with col_title: st.title("Age Distribution")
+if options == 'Sobre':
+    st.markdown("""
+    # Trabalho da disciplina Visualização de dados - PPGC
+    ### Utilizando dados do State of Data, fazemos comparações entre gêneros.
+    #### Grupo: Cassiano Michel, Emílio Bresolin, Marco Bochernitsan e Rodrigo Henrich
+    #### Professora: Isabel Harb Manssour
+                     
+    Comparações realizadas no trabalho:
+    - Idade
+    - Educação
+    - Salário
+                
+
+    ***Confira o [repositório no GitHub](https://github.com/marconb1/dataviz_masters) para mais informações.***
+    """)
+    _,col_title = st.columns([1, 1.9])
+    with col_title: 
+
+        script_directory = os.path.dirname(os.path.abspath(__file__))
+
+        folder_path = os.path.join(script_directory, 'images')
+        file_path = os.path.join(folder_path, 'date-everywhere-data.gif')
+        file_ = open(file_path, "rb")
+
+        contents = file_.read()
+        data_url = base64.b64encode(contents).decode("utf-8")
+        file_.close()
+
+        st.markdown(
+            f'<img src="data:image/gif;base64,{data_url}" alt="data_gif">',
+            unsafe_allow_html=True,
+        )
+
+
+if options == 'Idade':
+    _,col_title,_ = st.columns([1,1, 1])
+    with col_title: st.title("Distribuição de idades")
+    
+
+    st.markdown(
+        """
+        <style>
+        .some-space {
+            height: 40px; 
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.markdown('<div class="some-space"></div>', unsafe_allow_html=True)
+
+    st.markdown("""Nesse primeiro gráfico temos números brutos, e entendemos que para uma melhor comparação, faz mais sentido normalizar as quantidades, visto que há aproximadamente 3x mais homens do que mulheres respondendo a pesquisa""")
 
     st.plotly_chart(plot_age_distribution_by_gender(df_clean),use_container_width=True)
 
@@ -35,13 +88,33 @@ if options == 'Age Distribution':
         st.plotly_chart(plot_age_distribution_pie_chart(df_clean,'Feminino'),use_container_width=True)
 
         
-if options == 'Education level':
-    _,col_title = st.columns([1, 1.2])
-    with col_title: st.title("Education Level")
+if options == 'Educação':
+    _,col_title,_ = st.columns([1,1, 1])
+    with col_title: st.title("Nível de ensino")
 
     st.plotly_chart (plot_stacked_bar_percentage_education_level_by_gender(df_clean),use_container_width=True)
 
-if options == 'Salary Distribution':
-     _,col_title = st.columns([1, 1.2])
-     with col_title: st.title("Salary Distribution")
-     st.plotly_chart (plot_stacked_bar_percentage_salary_range_by_gender(df_clean),use_container_width=True)
+if options == 'Salário':
+    faixa_salarial_order = [
+    'Menos de R$ 1.000/mês',
+    'de R$ 101/mês a R$ 2.000/mês',
+    'de R$ 1.001/mês a R$ 2.000/mês',
+    'de R$ 2.001/mês a R$ 3.000/mês',
+    'de R$ 3.001/mês a R$ 4.000/mês',
+    'de R$ 4.001/mês a R$ 6.000/mês',
+    'de R$ 6.001/mês a R$ 8.000/mês',
+    'de R$ 8.001/mês a R$ 12.000/mês',
+    'de R$ 12.001/mês a R$ 16.000/mês',
+    'de R$ 16.001/mês a R$ 20.000/mês',
+    'de R$ 20.001/mês a R$ 25.000/mês',
+    'de R$ 25.001/mês a R$ 30.000/mês',
+    'de R$ 30.001/mês a R$ 40.000/mês',
+    'Acima de R$ 40.001/mês']
+    _,col_title,_ = st.columns([1,1, 1])
+    with col_title: st.title("Distribuição de salário por gênero")
+    st.plotly_chart (plot_stacked_bar_percentage_salary_range_by_gender(df_clean),use_container_width=True)
+
+    salary_category = st.selectbox("Selecione a Faixa Salarial", faixa_salarial_order, key="faixa_salarial")
+    gen_selector = st.selectbox("Selecione o gênero", ['todos','Masculino','Feminino'], key="gen_selector")
+    
+    st.plotly_chart(plot_mapa(df_clean,salary_category,gen_selector),use_container_width=True,height=700)
